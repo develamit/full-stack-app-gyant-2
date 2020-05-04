44fc2f5a90da753aff50c87bd2967d3e774004d9
develamit <develamit@gmail.com>
2020-05-03 23:54:37 -0700

    pushing cases and conditions to FE

--- public/client.js
+++ public/client.js
@@ -13,10 +13,18 @@ $(document).ready(function(){
     function(data, status){
       console.log("Status: ", status);
       console.log("cases: ", data.cases);
+      console.log("caseDesc: ", data.cases.caseDesc);
       console.log("conditions: ", data.conditions);
       if (status) {
         $(".container").hide();
         $("#case-cond").show();
+        $("#case-quote").html(data.cases.caseDesc);
+
+        $.each( data.conditions, function( index, value ){
+          console.log(value.code, " : ", value.desc);
+          $('.list-group').append('<li>' + value.code + ' || ' + value.desc + '</li>');
+          //<a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
+        });
 
       }
     });
