1b1240bb5498db8942ee504f199f7c8e64f20f97
develamit <develamit@gmail.com>
2020-05-04 01:04:37 -0700

    modified jquery for FE

--- public/client.js
+++ public/client.js
@@ -19,14 +19,39 @@ $(document).ready(function(){
         $(".container").hide();
         $("#case-cond").show();
         $("#case-quote").html(data.cases.caseDesc);
+        $("#logged-in-as").html('Logged in as: ' + userId + ' | <a href="#" id="log-out-id" class="log-out-class">Log Out</a>');
 
         $.each( data.conditions, function( index, value ){
           console.log(value.code, " : ", value.desc);
-          $('.list-group').append('<li>' + value.code + ' || ' + value.desc + '</li>');
+          //$('.list-group').append('<li>' + value.code + ' || ' + value.desc + '</li>');
+          $('.list-group').append('<a href="#" id="' + value.code + '" class="list-group-item list-group-item-action">' + value.code + ' || ' + value.desc + '</a>');
           //<a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
         });
 
       }
     });
   });
+
+
+  //$("#log-out-id").click(function(){
+    //console.log('log out clicked');
+    //$("#case-cond").hide();
+    //$("#signupbox").hide();
+    //$("#loginbox").show();
+  //});
+
+  //$("#log-out-id").on("click", function(){
+    //console.log('log out clicked');
+    //$("#case-cond").hide();
+    //$("#signupbox").hide();
+    //$("#loginbox").show();
+  //});
+
+  $('.log-out-class').attr("onclick", function(){
+    console.log('log out clicked');
+    $("#case-cond").hide();
+    $("#signupbox").hide();
+    $("#loginbox").show();
+  });
+
 });
