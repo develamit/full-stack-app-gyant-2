df16bb9b4846f279f9550652df8ff0f0020987b2
develamit <develamit@gmail.com>
2020-05-05 17:52:25 -0700

    added sign-up api. displaying messages. improved code base

--- public/client.js
+++ public/client.js
@@ -1,10 +1,43 @@
 
 $(document).ready(function(){
 
+
+  //=======================================================================
+  // jQuery function when login button is clicked
+  //   0. Calls the api /api/signup
+  //   1. Cretes the user, if not found
+  //   2. Displays appropriate messages back to the user
+  //=======================================================================
+  $("#btn-signup").click(function(){
+    let userId = $("#signup-username").val();
+    let passwd = $("#signup-password").val();
+    console.log('signup button was clicked: userId: ', userId, 'passwd: ', passwd);
+    $.post("/api/signup",
+    {
+      userId: userId,
+      passwd: passwd
+    },
+    function(data, status){
+      console.log("Signup Status: ", status, typeof status);
+      console.log("Signup data: ", data);
+
+      if (data == 'created') {
+        //$("#signup-result-id").html('Registration successful. Please Sign In');
+        $("#signup-result-id").html('<a href="#" id="sign-in-id" class="log-out-class">Registration successful. Please Sign In</a>');
+      } else if (data == 'exists') {
+        $("#signup-result-id").html('User exists. Please try a different userId');
+      }
+
+    });
+
+  });
+
+
+
   //=======================================================================
   // jQuery function when login button is clicked
   //   0. Calls the api /api/login
-  //   1. Verifies the admin userId
+  //   1. Verifies the userId
   //   2. Takes to the case, condition page
   //   3. Registers all the conditions in localStorage - ONE TIME
   //   4. Preserves the previous case to condition mapping, if any
@@ -12,8 +45,9 @@ $(document).ready(function(){
   $("#btn-login").click(function(){
     let userId = $("#login-username").val();
     let passwd = $("#login-password").val();
-    console.log('button was clicked: userId: ', userId, 'passwd: ', passwd);
-    localStorage.setItem('listId', "-1");
+    console.log('login button was clicked: userId: ', userId, 'passwd: ', passwd);
+
+    localStorage.setItem('listId', "-1"); // initialization
 
     $.post("/api/login",
     {
@@ -22,60 +56,88 @@ $(document).ready(function(){
     },
     function(data, status){
       console.log("Status: ", status);
+      console.log("Message: ", data.message);
       console.log("cases: ", data.cases);
-      //console.log("caseDesc: ", data.cases.caseDesc);
-      //console.log("conditions: ", data.conditions);
-      let caseCode = data.cases.code; // Get the code associated with the case
-      console.log('caseCode: ', caseCode);
-      if (status) {
-        $(".container").hide();
-        $("#case-cond").show();
-        $("#done-cond").hide();
-        $("#case-quote").html(data.cases.caseDesc);
-        $("#case-footer").html('Case Id: ' + data.cases.caseId + ' Sequence: ' + data.cases.seq);
-        $("#logged-in-as").html('Logged in as: ' + userId + ' | <a href="#" id="log-out-id" class="log-out-class">Log Out</a>');
-        //$("#logged-in-as").html('Logged in as: ' + userId + ' | <button id="log-out-id" type="button" class="btn btn-info"><i class="icon-hand-right"></i>Log Out</button>');
 
-        // store the case sequence number in local storage
-        localStorage.setItem('caseIdSeqNum', data.cases.seq);
+      // sanity Check
+      if (data.message == "invalid") {
+        $("#login-result-id").show();
+        $("#login-result-id").html('User not found. Please Sign up');
 
-        // One Time Process during login: List all the conditions in a list-group-item
-        let listItems = $(".list-group-item");
-        console.log('Num listItems: ', listItems.length);
-        if (listItems.length < 1) {
-          $.each( data.conditions, function( index, value ){
-            //console.log(value.code, " : ", value.desc);
-            localStorage.setItem(value.code, index); // One Time - store a map of condition code : index Position
-            if (caseCode != "-1" && value.code == caseCode) { // This case has a meaningful condition code - from a previous selection
-              localStorage.setItem('listId', caseCode);
-              localStorage.setItem('listIdIdx', index);
-              $('.list-group').append('<a href="#" id="' + value.code + '" class="list-group-item list-group-item-action active">' + value.code + ' || ' + value.desc + '</a>');
-            } else {
-              $('.list-group').append('<a href="#" id="' + value.code + '" class="list-group-item list-group-item-action">' + value.code + ' || ' + value.desc + '</a>');
-            }
-          });
-        } else {
+      } else if (data.message == "passwd-incorrect") {
+        $("#login-result-id").show();
+        $("#login-result-id").html('Incorrect password. Please try again');
 
-            // Remove any previous active list items
-            prevListIdxPos = localStorage.getItem('listIdIdx');
-            console.log('prevListIdxPos: ', prevListIdxPos);
-            if (prevListIdxPos) {
-              let listItems = $(".list-group-item");
-              listItems[prevListIdxPos].classList.remove("active");
-            }
+      } else if (data.message == "valid") {
 
-            if (caseCode != "-1") {
-              let codeIdx = localStorage.getItem(caseCode);
-              if (codeIdx) {
-                let listItems = $(".list-group-item");
-                listItems[codeIdx].classList.add("active");
+        //console.log("caseDesc: ", data.cases.caseDesc);
+        //console.log("conditions: ", data.conditions);
+        let caseCode = data.cases.code; // Get the code associated with the case
+        let caseId = Math.trunc(data.cases.caseId * 1000);
+        let lastModified = Math.trunc(data.cases.lastModified * 1000);
+        let lastModDate = ""
+        //if (caseId != lastModified) {
+          let d = new Date(lastModified);
+          lastModDate = d.toLocaleString();
+          console.log('Case last modified on: ', lastModDate);
+        //}
+        console.log('type: ', caseId, lastModified)
+        console.log('caseCode: ', caseCode);
+        if (status) {
+          $(".container").hide();
+          $("#case-cond").show();
+          $("#btn-next").attr('disabled', false);
+          $("#done-cond").hide();
+          $("#case-quote").html(data.cases.caseDesc);
+          $("#case-footer").html('Case Id: ' + data.cases.caseId + ' Sequence: ' + data.cases.seq);
+          $("#logged-in-as").html('Logged in as: ' + userId + ' | <a href="#" id="log-out-id" class="log-out-class">Log Out</a>');
+          //$("#logged-in-as").html('Logged in as: ' + userId + ' | <button id="log-out-id" type="button" class="btn btn-info"><i class="icon-hand-right"></i>Log Out</button>');
+
+          // store the case sequence number in local storage
+          localStorage.setItem('caseIdSeqNum', data.cases.seq);
+          localStorage.setItem(data.cases.seq, null); // initializing case modified time to null
+
+          // One Time Process during login: List all the conditions in a list-group-item
+          let listItems = $(".list-group-item");
+          console.log('Num listItems: ', listItems.length);
+          if (listItems.length < 1) {
+            $.each( data.conditions, function( index, value ){
+              //console.log(value.code, " : ", value.desc);
+              localStorage.setItem(value.code, index); // One Time - store a map of condition code : index Position
+              if (caseCode != "-1" && value.code == caseCode) { // This case has a meaningful condition code - from a previous selection
                 localStorage.setItem('listId', caseCode);
-                localStorage.setItem('listIdIdx', codeIdx);
+                localStorage.setItem('listIdIdx', index);
+                $('.list-group').append('<a href="#" id="' + value.code + '" class="list-group-item list-group-item-action active">' + value.code + ' || ' + value.desc + '</a>');
+              } else {
+                $('.list-group').append('<a href="#" id="' + value.code + '" class="list-group-item list-group-item-action">' + value.code + ' || ' + value.desc + '</a>');
               }
-            }
+            });
+          } else {
+
+              // Remove any previous active list items
+              prevListIdxPos = localStorage.getItem('listIdIdx');
+              console.log('prevListIdxPos: ', prevListIdxPos);
+              if (prevListIdxPos) {
+                let listItems = $(".list-group-item");
+                listItems[prevListIdxPos].classList.remove("active");
+              }
+
+              if (caseCode != "-1") {
+                let codeIdx = localStorage.getItem(caseCode);
+                if (codeIdx) {
+                  let listItems = $(".list-group-item");
+                  listItems[codeIdx].classList.add("active");
+                  localStorage.setItem('listId', caseCode);
+                  localStorage.setItem('listIdIdx', codeIdx);
+                }
+              }
+          }
+
         }
 
-      }
+
+    } // end of else
+
     });
   });
 
@@ -92,7 +154,7 @@ $(document).ready(function(){
   $("#btn-next").click(function(){
     listId = localStorage.getItem('listId');
     curCaseSeqNum = localStorage.getItem('caseIdSeqNum');
-    console.log('btn-next clicked: localstorage[listId] = ', listId, 'curCaseSeqNum = ', curCaseSeqNum);
+    console.log('btn-next clicked: localstorage[listId] = ', listId, 'curCaseSeqNum = ', curCaseSeqNum, 'caseModified on: ', localStorage.getItem(curCaseSeqNum));
 
     // Remove any previous active list items
     prevListIdxPos = localStorage.getItem('listIdIdx');
@@ -100,6 +162,17 @@ $(document).ready(function(){
     if (prevListIdxPos) {
       let listItems = $(".list-group-item");
       listItems[prevListIdxPos].classList.remove("active");
+    }
+
+    // extract the last modified time for the current case
+    let lastModified = Number(localStorage.getItem(curCaseSeqNum));
+    console.log('lastModified2: ', lastModified, typeof lastModified);
+    if (isNaN(lastModified)) {
+      console.log('lastModified2 is NaN');
+    } else {
+      let d = new Date(lastModified);
+      lastModDate = d.toLocaleString();
+      console.log('Current Case last modified on: ', lastModDate);
     }
 
     $.post("/api/nextcase",
@@ -128,8 +201,10 @@ $(document).ready(function(){
         $("#case-footer").html('Case Id: ' + data.cases.caseId + ' Sequence: ' + data.cases.seq);
         // store the case sequence number in local storage
         localStorage.setItem('caseIdSeqNum', data.cases.seq);
+        localStorage.setItem(curCaseSeqNum, null); // initializing case modified time to null
       } else{
         console.log("cases: ", data.cases, " Mark DONE");
+        $("#btn-next").attr('disabled', true);
         $("#done-cond").show();
         $("#card-done-id").html('<a href="#" id="log-out-id" class="log-out-class">You are Done. Click to log out</a>');
       }
@@ -172,13 +247,18 @@ $(document).ready(function(){
     let listId   = $(this).attr('id');
     localStorage.setItem('listId', listId);
     localStorage.setItem('listIdIdx', indexPos);
-    console.log('list-group clicked: index = ', indexPos, 'id: ', listId);
+    let curCaseSeqNum = localStorage.getItem('caseIdSeqNum');
+    let caseModifiedOn = Date.now();
+    localStorage.setItem(curCaseSeqNum, caseModifiedOn); // storing when the case was modified
+
+    console.log('list-group clicked: index = ', indexPos, 'id: ', listId, 'case seq:', curCaseSeqNum, 'modified on: ', caseModifiedOn);
     $(this).addClass("active");
   });
 
 
   //=======================================================================
-  // jQuery function when logout is clicked
+  // jQuery function when "You are Done" is clicked. This simply takes the
+  // user back to the login page
   // Using the .on() method to capture click events on dynamically created
   // elements by jQuery
   //=======================================================================
@@ -188,9 +268,32 @@ $(document).ready(function(){
     $("#case-cond").hide();
     $("#done-cond").hide();
     $("#signupbox").hide();
+    //$(".container").show();
+    $("#signupbox").hide();
+    $("#login-result-id").hide();
     $(".container").show();
-    //$("#login-username").val('');
-    //$("#login-password").val('');
+    //$('#loginbox').show();
+    $("#login-username").val('');
+    $("#login-password").val('');
+  });
+
+
+  //=======================================================================
+  // jQuery function when sign-in is clicked from registraion page footer
+  // This simply takes the user back to the login page
+  // Using the .on() method to capture click events on dynamically created
+  // elements by jQuery
+  //=======================================================================
+
+  $("div").on("click", "a#sign-in-id", function(){
+    console.log('sign-in clicked');
+    $("#singnup-username").val('');
+    $("#signup-password").val('');
+    $("#login-username").val('');
+    $("#login-password").val('');
+    $("#signupbox").hide();
+    $("#login-result-id").hide();
+    $('#loginbox').show();
   });
 
 });
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const Express = require("express");
+const express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 const path = require('path');
 const bcrypt = require('bcrypt');
-const saltRounds = 1;
+const saltRounds = 1; // can be increased, at the expense of time
 const CONNECTION_URL = 'mongodb://localhost:27017/';
 const DATABASE_NAME = "healthrecord";
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 
-const app = Express();
-const router = Express.Router();
+const app = express();
+const router = express.Router();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
-//app.use(Express.json())
 let database, collectionCases, collectionConditions;
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
 
+//-----------------------------------------------------------------------------------
+// server listening on port 8080 of host 0.0.0.0, which will also work from localhost
+//-----------------------------------------------------------------------------------
 app.listen(PORT, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
         if(error) {
@@ -37,7 +39,13 @@ app.get("/", (request, response) => {
     response.render('login');
 });
 
-
+//-----------------------------------------------------------------------------------
+// api endpoint: /api/signup
+// Function:
+//    1. Check if the user exists
+//    2. Creates the user and encrypts the passwd if the user does not exist
+//    3. Returns either 'created' or 'exists' message
+//-----------------------------------------------------------------------------------
 app.post("/api/signup", (request, response) => {
     console.log('/api/signup request body: ', request.body);
     const userId = request.body.userId;
@@ -53,33 +61,44 @@ app.post("/api/signup", (request, response) => {
           if (!result) { // create the user, since record is not found
             bcrypt.hash(plainPasswd, saltRounds, function(err, hash) {
               // Store hash in your password DB.
-              // TODO - use timestamp for 'id' and 'lastModified' field
-              let insertStr = {"userId" : userId, "passwd": hash};
+              // use timestamp for 'createdOn' and 'lastUpdatedOn' field
+              let createdOn = Date.now();
+              let insertStr = {"userId" : userId, "passwd": hash, "createdOn":createdOn, "lastUpdatedOn":createdOn};
               console.log('hash: ', hash);
               collectionUser.insertOne(insertStr, (error, result) => {
                   if(error) {
                       return response.status(500).send(error);
                   }
                   console.log('user created successfully');
-                  response.send(result);
+                  response.send('created');
               });
 
             });
           }
           else { // the user exists. Redirect him to the login page // TODO
             console.log('user: ', userId, ' exists. Please login');
-            response.send(result);
+            response.send('exists');
           }
         }
     });
 });
+
+//-----------------------------------------------------------------------------------
+// api endpoint: /api/login
+// Function:
+//    1. Check if the user is valid - password comparison is done using bcrypt module
+//    2. Finds the first entry in the DB cases collection
+//    3. Finds the list of conditions from the DB
+//    4. Sends back the response as {first case, [conditions]}
+// Note: Since this api calls MongoDB multiple times, "promise" is used to avoid
+//       callback hell
+//-----------------------------------------------------------------------------------
 
 app.post("/api/login", (request, response) => {
     console.log('/api/login request body: ', request.body);
     const userId = request.body.userId;
     const plainPasswd = request.body.passwd;
     let queryStr = {"userId" : userId};
-    //let projection = {"passwd" : 1};
     let queries = [
         collectionUser.findOne(queryStr),
         collectionCases.findOne({}),
@@ -89,13 +108,14 @@ app.post("/api/login", (request, response) => {
       Promise.all(queries)
         .then(results => {
           if (!results[0]) {
-            console.log("Error in user");
+            console.log("Error in querying user in DB");
+          }
+          if (!results[1]) {
+            console.log("Error in querying cases in DB");
             return response.status(500).send(error);
-          } else if (!results[1]) {
-            console.log("Error in cases");
-            return response.status(500).send(error);
-          } else if (!results[2]) {
-            console.log("Error in conditions");
+          }
+          if (!results[2]) {
+            console.log("Error in querying conditions in DB");
             return response.status(500).send(error);
           }
 
@@ -107,25 +127,53 @@ app.post("/api/login", (request, response) => {
           console.log('casesData: ', casesData);
           console.log('conditionsData: ', conditionsData);
 
-          bcrypt.compare(plainPasswd, userData.passwd, function(err, res) {
-            if(res) {
-              console.log('passwd match');
-              response.send({"cases" : casesData, "conditions" : conditionsData});
-            }
-          });
+          //---------------------------------------------------------------
+          // validation checks - return appropriate message to FE
+          //---------------------------------------------------------------
+          if (!userData) {
+            console.log('invalid user');
+            response.send({"cases" : null, "conditions" : null, "message" : "invalid"});
+          }
+          if (!casesData) {
+            console.log('cases not found');
+            response.send({"cases" : null, "conditions" : null, "message" : "cases-not-found"});
+          }
+          if (!conditionsData) {
+            console.log('conditions not found');
+            response.send({"cases" : null, "conditions" : null, "message" : "conditions-not-found"});
+          }
+
+          //---------------------------------------------------------------
+          // everything is valid at this point. Return first case and all conditions
+          //---------------------------------------------------------------
+          if (userData) {
+            bcrypt.compare(plainPasswd, userData.passwd, function(err, res) {
+              if(res) {
+                console.log('passwd match');
+                response.send({"cases" : casesData, "conditions" : conditionsData, "message" : "valid"});
+              }
+              else {
+                console.log('passwd incorrect');
+                response.send({"cases" : null, "conditions" : null, "message" : "passwd-incorrect"});
+              }
+            });
+          }
 
         })
         .catch(err => {
           console.log("Error in getting queries", err);
-          return response.status(500).send(error);
+          return response.status(500).send(err);
       });
 
   });
 
 
+
+//-----------------------------------------------------------------------------------
 // This does 2 mongoDB tasks:
 //        1. Record the condition for the current case
 //        2. Send the next case to FE
+//-----------------------------------------------------------------------------------
 app.post("/api/nextcase", (request, response) => {
     console.log('/api/nextcase request body: ', request.body);
     const curSeq = parseInt(request.body.curSeq, 10);
@@ -146,10 +194,8 @@ app.post("/api/nextcase", (request, response) => {
         .then(results => {
           if (!results[0]) {
             console.log("Update did not happen during updateOne operation");
-            //response.send({"cases" : null});
           } else if (!results[1]) {
             console.log("Cases not found during findOne operation");
-            //response.send({"cases" : null});
           }
 
           let updateResult       = results[0];
@@ -165,14 +211,13 @@ app.post("/api/nextcase", (request, response) => {
         })
         .catch(err => {
           console.log("Error in updating and getting next case, error code: ", err);
-          //response.send({"cases" : null});
           return response.status(500).send(err);
         });
 
 });
 
 
-
+/* DELETE LATER
 app.get("/api/cases", (request, response) => {
     collectionCases.find({}).toArray((error, result) => {
         if(error) {
@@ -191,6 +236,8 @@ app.get("/api/conditions", (request, response) => {
         response.send(result);
     });
 });
+*/
+
 
 app.use('/api', router);
-app.use(Express.static('public'));
+app.use(express.static('public'));
